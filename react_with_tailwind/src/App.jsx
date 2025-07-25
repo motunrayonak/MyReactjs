7826c5fcaeee6753a41be7f1014f867482cc4d49
motunrayonak <motunrayonak@gmail.com>
2025-07-25 18:55:17 +0100

    first tailwind created

---
+++ react_with_tailwind/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <p className='bg-blue-500'>Hello world</p>
+    </div>
+  )
+}
+
+export default App
